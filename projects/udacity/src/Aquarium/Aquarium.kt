5e356057197e844434a68e27a60684e961737143
baurine <2008.hbl@gmail.com>
2018-10-14 11:23:45 +0800

    Class inherit

--- projects/udacity/src/Aquarium/Aquarium.kt
+++ projects/udacity/src/Aquarium/Aquarium.kt
@@ -1,14 +1,29 @@
 package Aquarium
 
-class Aquarium(var width: Int = 20, var length: Int = 40, var height: Int = 100) {
-    var volume: Int
+import kotlin.math.PI
+
+open class Aquarium(var width: Int = 20, var length: Int = 40, var height: Int = 100) {
+    open var volume: Int
         get() = width*length*height/1000
         set(value) {
             height = (value*1000)/(width*length)
         }
 
-    var water = volume * 0.9
+    open var water = volume * 0.9
 
     constructor(numberOfFish: Int) : this() {
+        val water = numberOfFish * 2000
+        val tank = water + water * 0.1
+        height = (tank / (length*width)).toInt()
     }
+}
+
+class TowerTank() : Aquarium() {
+    override var water = volume * 0.8
+
+    override var volume: Int
+        get() = (width*height*length/1000* PI).toInt()
+        set(value) {
+            height = (value*1000)/(width*length)
+        }
 }

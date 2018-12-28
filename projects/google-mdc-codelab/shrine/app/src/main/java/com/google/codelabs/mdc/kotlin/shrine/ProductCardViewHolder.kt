b5abaa2b44c79304805b0637eed9d2e00e94ee2f
baurine <2008.hbl@gmail.com>
2018-12-28 17:02:15 +0800

    MDC102 - ProductList by RecyclerView

--- projects/google-mdc-codelab/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardViewHolder.kt
+++ projects/google-mdc-codelab/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardViewHolder.kt
@@ -2,6 +2,12 @@ package com.google.codelabs.mdc.kotlin.shrine
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.TextView
+import com.android.volley.toolbox.NetworkImageView
 
 class ProductCardViewHolder(itemView: View) //TODO: Find and store views from itemView
-    : RecyclerView.ViewHolder(itemView)
+    : RecyclerView.ViewHolder(itemView) {
+    var productImage = itemView.findViewById<NetworkImageView>(R.id.product_image)
+    var productTitle = itemView.findViewById<TextView>(R.id.product_title)
+    var productPrice = itemView.findViewById<TextView>(R.id.product_price)
+}
--- projects/google-mdc-codelab/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ projects/google-mdc-codelab/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -3,7 +3,9 @@ package com.google.codelabs.mdc.kotlin.shrine
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
 import android.view.*
+import com.google.codelabs.mdc.kotlin.shrine.network.ProductEntry
 import kotlinx.android.synthetic.main.shr_product_grid_fragment.view.*
 
 class ProductGridFragment : Fragment() {
@@ -20,7 +22,19 @@ class ProductGridFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // setup toolbar
         (activity as AppCompatActivity).setSupportActionBar(view.toolbar)
+
+        // setup recycler view
+        val productAdapter = ProductCardRecyclerViewAdapter(ProductEntry.initProductEntryList(resources))
+        view.recycler_view.apply {
+            setHasFixedSize(true)
+            layoutManager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
+            adapter = productAdapter
+            val largePadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing)
+            val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
+            addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
